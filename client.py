a002691fd3b19dff7639f12a021cee21d7c8d347
kubapok <jakubpokrywka@gmail.com>
2017-01-08 11:22:12 +0100

    przed przejsciem na udp

--- client.py
+++ client.py
@@ -1,11 +1,12 @@
-#client example
+
 import socket
 import sys
 import pyaudio
 import wave
 
+
 PORT = int(sys.argv[1])
-CHUNK =  50000
+CHUNK =  12000
 AUDIOCHUNK = 3000
 
 FORMAT = 8
@@ -29,21 +30,35 @@ s_broadcast.connect(('localhost', PORT))
 s_listen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s_listen.connect(('localhost', PORT))
 
-p = pyaudio.PyAudio()
-stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,output=OUTPUT)
-wf = wave.open(music_file, 'rb')
+# import fcntl, os
+# fcntl.fcntl(s_request, fcntl.F_SETFL, os.O_NONBLOCK)
+# fcntl.fcntl(s_command, fcntl.F_SETFL, os.O_NONBLOCK)
+# fcntl.fcntl(s_broadcast, fcntl.F_SETFL, os.O_NONBLOCK)
+# fcntl.fcntl(s_listen, fcntl.F_SETFL, os.O_NONBLOCK)
+
+if sys.argv[2] == 'broadcast':
+    wf = wave.open(music_file, 'rb')
+    print('broadcast')
+else:
+    print('listen')
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,output=OUTPUT)
 
 
 while True:
     s_request.sendall(bytes('listen','ascii'))
     command = s_command.recv(CHUNK)
-    print(str(command,'ascii'))
+    #print(str(command,'ascii'))
 
     if str(command,'ascii') == "broadcast":
         audio_data = wf.readframes(AUDIOCHUNK)
         s_broadcast.sendall(audio_data)
+        print(audio_data[0:10])
+        print(len(audio_data))
     elif str(command,'ascii') == "listen":
-        audio_data = s_listen.recv(CHUNK)[:]
-        # stream.write(audio_data)
+        audio_data = s_listen.recv(CHUNK)
+        print(audio_data[0:10])
+        print(len(audio_data))
+        stream.write(audio_data)
     else:
         print('sth unexpected happend')
--- server.py
+++ server.py
@@ -3,14 +3,20 @@ import socket
 import sys
 import time
 
-HOST = ''   # Symbolic name, meaning all available interfaces
-PORT = int(sys.argv[1]) # Arbitrary non-privileged port
+HOST = ''
+PORT = int(sys.argv[1])
 CHUNK =  50000
+CLIENTQUANTITY = 2
+
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
 
-#Bind socket to local host and port
+
+# import fcntl, os
+# fcntl.fcntl(s, fcntl.F_SETFL, os.O_NONBLOCK)
+
 try:
     s.bind((HOST, PORT))
 except socket.error as msg:
@@ -19,10 +25,8 @@ except socket.error as msg:
 
 print('Socket bind complete')
 
-
-s.listen(10)
+s.listen(20)
 print('Socket now listening')
-
 
 class Client():
     all = list()
@@ -31,58 +35,97 @@ class Client():
         self.c_command, self.addr_command = s.accept()
         self.c_broadcast, self.addr_broadcast = s.accept()
         self.c_listen, self.addr_listen = s.accept()
-        Client.all.append(self)
+
+        self.isAlive = True
+
+        self.request = None
+        self.command = None
+
+    def connectClients():
+        for i in range(CLIENTQUANTITY):
+            client = Client()
+            Client.all.append(client)
+
+    def receiveRequests():
+        for client in Client.all:
+            if client.isAlive:
+                client.request = str(client.c_request.recv(CHUNK),'ascii')
+
+    def setCommands():
+        #for i in range(3):
+        Client.all[0].command = 'broadcast'
+        Client.all[1].command = 'listen'
+
+    def sendCommands():
+        for client in Client.all:
+            if client.isAlive:
+                client.c_command.sendall(bytes(client.command, 'ascii'))
 
 
+    def recvAudio():
+        for client in Client.all:
+            if client.isAlive and client.command == 'broadcast':
+                Client.audio_data = client.c_broadcast.recv(CHUNK)
+                break
 
-client1 = Client()
-client2 = Client()
-client3 = Client()
-# client4 = Client()
+    def sendAudio():
+        for client in Client.all:
+            if client.isAlive:
+                client.c_listen.sendall(Client.audio_data)
 
 
-import time
-t_start = time.time()
-notification = True
-sw = False
+Client.connectClients()
+
+# import time
+# t_start = time.time()
+# notification = True
+# sw = False
+
 while True:
-    if notification and time.time() - t_start > 4:
-        notification = False
-        print('GOWWWWNO\n' * 60 )
-        sw = True
+    # if notification and 5 > time.time() - t_start > 4:
+    #     notification = False
+    #     print('GOWWWWNO\n' * 60 )
+    #     sw = True
+    #
+    # if  time.time() - t_start > 8:
+    #     print('SEEER\n' * 60 )
+    #
+    #     sw = False
 
 
-    request1 = client1.c_request.recv(CHUNK)
-    request2 = client2.c_request.recv(CHUNK)
-    request3 = client3.c_request.recv(CHUNK)
-    # request4 = client4.c_request.recv(CHUNK)
-    print(str(request1,'ascii'))
-    print(str(request2,'ascii'))
-    print(str(request3,'ascii'))
-    # print(str(request4,'ascii'))
+    Client.receiveRequests()
+    Client.setCommands()
+    Client.sendCommands()
+    print('command sent')
+    Client.recvAudio()
+    print('audio receinved')
+    print(Client.audio_data[0:10])
+    print(len(Client.audio_data))
+    Client.sendAudio()
+    print('audio sent')
 
 
-    if not sw:
-        client1.c_command.sendall(bytes('broadcast', 'ascii'))
-        client2.c_command.sendall(bytes('listen', 'ascii'))
-        client3.c_command.sendall(bytes('listen', 'ascii'))
-        # client4.c_command.sendall(bytes('listen', 'ascii'))
-
-
-        audio_data = client1.c_broadcast.recv(CHUNK)
-        client2.c_listen.sendall(audio_data)
-        client3.c_listen.sendall(audio_data)
-        # client4.c_listen.sendall(audio_data)
-    else:
-        client1.c_command.sendall(bytes('listen', 'ascii'))
-        client2.c_command.sendall(bytes('broadcast', 'ascii'))
-        client3.c_command.sendall(bytes('listen', 'ascii'))
-        # client4.c_command.sendall(bytes('listen', 'ascii'))
-
-
-        audio_data = client2.c_broadcast.recv(CHUNK)
-        client1.c_listen.sendall(audio_data)
-        client3.c_listen.sendall(audio_data)
-        # client4.c_listen.sendall(audio_data)
+    # if not sw:
+    #     client1.c_command.sendall(bytes('broadcast', 'ascii'))
+    #     client2.c_command.sendall(bytes('listen', 'ascii'))
+    #     client3.c_command.sendall(bytes('listen', 'ascii'))
+    #     client4.c_command.sendall(bytes('listen', 'ascii'))
+    #
+    #
+    #     audio_data = client1.c_broadcast.recv(CHUNK)
+    #     client2.c_listen.sendall(audio_data)
+    #     client3.c_listen.sendall(audio_data)
+    #     client4.c_listen.sendall(audio_data)
+    # else:
+    #     client1.c_command.sendall(bytes('listen', 'ascii'))
+    #     client2.c_command.sendall(bytes('broadcast', 'ascii'))
+    #     client3.c_command.sendall(bytes('listen', 'ascii'))
+    #     client4.c_command.sendall(bytes('listen', 'ascii'))
+    #
+    #
+    #     audio_data = client2.c_broadcast.recv(CHUNK)
+    #     client1.c_listen.sendall(audio_data)
+    #     client3.c_listen.sendall(audio_data)
+    #     client4.c_listen.sendall(audio_data)
 
 s.close()
