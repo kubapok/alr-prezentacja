ffce882f484dec200ee8f91d80efa2a744b7dbce
kubapok <jakubpokrywka@gmail.com>
2017-01-09 02:44:36 +0100

    some logging added

--- client_logic.py
+++ client_logic.py
@@ -3,10 +3,10 @@ import socket
 import sys
 import pyaudio
 import wave
+import os
+import datetime
 
-
-
-PORT = 9999
+PORT = 9995
 CHUNK =  12000
 AUDIOCHUNK = 3000
 
@@ -34,16 +34,28 @@ wf = wave.open(music_file, 'rb')
 p = pyaudio.PyAudio()
 stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,output=OUTPUT)
 
+
+dir_list = os.listdir()
+for i in range(1,20):
+    if not str(i) in dir_list:
+        log_file = open(str(i), 'w+')
+        log_id = str(i)
+        break
+
+
 def action(request_state):
     s_request.sendall(bytes(request_state,'ascii'))
     command = str(s_command.recv(CHUNK),'ascii')
     print('command received: ', command)
     print('I have to', command)
 
+
     if command == "broadcast":
+        log_file.write(str(datetime.datetime.now())[-15:]+'-'+log_id+ '-broadcasting\n')
         audio_data = wf.readframes(AUDIOCHUNK)
         s_broadcast.sendall(audio_data)
     elif command == "listen":
+        log_file.write(str(datetime.datetime.now())[-15:]+'-'+log_id+ '-listening\n')
         audio_data = s_listen.recv(CHUNK)
         stream.write(audio_data)
     else:
